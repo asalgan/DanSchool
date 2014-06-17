69d9f57c8901e0ae7397881e785e9486759ec977
Alan Salganik <asalganik1@gmail.com>
2014-06-16 22:48:58 -0500

    added class pages

---
+++ app/controllers/classes_controller.rb
@@ -0,0 +1,13 @@
+class ClassesController < ApplicationController
+  
+  def intro_to_entrepreneurship
+  end
+
+  def public_speaking
+  end
+
+  def social_enterprise 
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ DanSchool::Application.routes.draw do
 	resources :contacts, :only => [:new, :create] do
   	get 'thank_you', :on => :collection
 	end
+
+	get '/classes/intro_to_entrepreneurship' => 'classes#intro_to_entrepreneurship'
+	get '/classes/public_speaking' => 'classes#public_speaking'
+	get '/classes/social_enterprise' => 'classes#social_enterprise'
 	
 end
