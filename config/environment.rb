a317871959b77761bb2626f31a49dad8db38e6a8
Alan Salganik <asalganik1@gmail.com>
2014-06-22 23:07:32 -0500

    email changes

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,8 @@ DanSchool::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
-  :domain => "my-sites-domain.com",
-  :address => "smtp.sendgrid.net",
+  :domain => "coursepioneer.com",
+  :address => "smtp.gmail.com",
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
