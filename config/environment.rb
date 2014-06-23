3226235952021e6ad4ada0398e0b32ff2c759f2e
Alan Salganik <asalganik1@gmail.com>
2014-06-22 23:10:47 -0500

    email changes

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,12 @@ DanSchool::Application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
-  :domain => "coursepioneer.com",
+  :domain => "danschool.herokuapp.com",
   :address => "smtp.gmail.com",
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
