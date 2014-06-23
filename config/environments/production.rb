cb84f1269e93f819a27b69fc78d3d6f70b997c92
Alan Salganik <asalganik1@gmail.com>
2014-06-23 16:45:54 -0500

    trying to get mailer to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,13 +75,13 @@ DanSchool::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "mail.google.com",
+    domain: "www.gmail.com",
     authentication: "plain",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'mail.google.com' }
+  config.action_mailer.default_url_options = { :host => 'danschool.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
