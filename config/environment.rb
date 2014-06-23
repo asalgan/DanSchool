171132fc490e17fbbfdcd1e0577c20e1a3585825
Alan Salganik <asalganik1@gmail.com>
2014-06-23 17:42:57 -0500

    trying to get mailer to work in production

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 DanSchool::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
-    domain: "www.gmail.com",
+    domain: "heroku.com",
     authentication: "plain",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,9 +73,9 @@ DanSchool::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net"
     port: 587,
-    domain: "www.gmail.com",
+    domain: "heroku.com",
     authentication: "plain",
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
