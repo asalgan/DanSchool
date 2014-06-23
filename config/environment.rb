586b9c238143512fce0974af38d71900f5bb6ff7
Alan Salganik <asalganik1@gmail.com>
2014-06-23 17:28:53 -0500

    trying to get mailer to work in production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 DanSchool::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 25,
+    domain: "www.gmail.com",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
