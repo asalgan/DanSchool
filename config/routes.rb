2418b060b412dce1487e039c54b98abbcc0b17ff
Alan Salganik <asalganik1@gmail.com>
2014-07-07 13:46:10 -0500

    added footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ DanSchool::Application.routes.draw do
 	get '/classes/intro_to_entrepreneurship' => 'classes#intro_to_entrepreneurship'
 	get '/classes/public_speaking' => 'classes#public_speaking'
 	get '/classes/social_enterprise' => 'classes#social_enterprise'
+	get '/blog' => 'blog#index'
 
 	# match '/contacts',     to: 'contacts#new',             via: 'get'
 	resources "contacts", only: [:new, :create]
